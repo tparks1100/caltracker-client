4cdede0490c01ff4380b731fd2d3b23c9f176de2
tparks1100 <thulaparks@gmail.com>
2020-08-10 11:20:17 -0700

    Changes in production URL
    back slash removed after com

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://secret-woodland-45312.herokuapp.com/',
+  production: 'https://secret-woodland-45312.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
